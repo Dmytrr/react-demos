b4415b39336ca5747ffaf3a15443b69b8279c99e
Dmytrr <dimonpavlo@yahoo.com>
2022-06-21 11:48:19 +0300

    react-hw-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Simpsons from "./components/Simpsons";
-import RickAndMorty from "./components/RickAndMorty";
-import {useEffect, useState} from "react";
 
 function App() {
-    let [chars, setChars] = useState([]);
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(json => {
-                setChars(json);
-            })
-    }, [])
-    console.log(chars.map(value=>value.name))
 
     return (
         <div className="wrap">
-            <div className="simpsons">
                 <Simpsons name={'Homer'}
                           source={'https://i.pinimg.com/originals/36/f3/76/36f376dcc85528273912ba4cfb54a1f2.png'}/>
                 <Simpsons name={'Marge'}
@@ -26,10 +14,6 @@ function App() {
                 <Simpsons name={'Lisa'}
                           source={'https://i.pinimg.com/originals/03/05/1e/03051e7071e4d8a8929dcee946244e7d.png'}/>
                 <Simpsons name={'Maggie'} source={'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'}/>
-            </div>
-            <div className="rickandmorty">
-                <RickAndMorty name={chars.map(value=>value.name)}/>
-            </div>
         </div>
     );
 }
