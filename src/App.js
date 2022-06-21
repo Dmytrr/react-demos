a1602f81e42de1172abdc909b00ab173c8f6e85b
Dmytrr <dimonpavlo@yahoo.com>
2022-06-21 14:57:26 +0300

    react

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 import RickAndMorty from "./components/RickAndMorty";
 
 function App() {
-    <div className={"wrap"}>
-        <RickAndMorty name={"Rick Sanchez"} id={1} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
-    </div>
+    return (
+        <div className={"wrap"}>
+            <RickAndMorty name={"Rick Sanchez"} id={1} status={"Alive"} species={"Human"} gender={"Male"}
+                          image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
+            <RickAndMorty name={"Morty Smith"} id={2} status={"Alive"} species={"Human"} gender={"Male"}
+                          image={"https://rickandmortyapi.com/api/character/avatar/2.jpeg"}/>
+            <RickAndMorty name={"Summer Smith"} id={3} status={"Alive"} species={"Human"} gender={"Female"}
+                          image={"https://rickandmortyapi.com/api/character/avatar/3.jpeg"}/>
+            <RickAndMorty name={"Beth Smith"} id={4} status={"Alive"} species={"Human"} gender={"Female"}
+                          image={"https://rickandmortyapi.com/api/character/avatar/4.jpeg"}/>
+            <RickAndMorty name={"Jerry Smith"} id={5} status={"Alive"} species={"Human"} gender={"Male"}
+                          image={"https://rickandmortyapi.com/api/character/avatar/5.jpeg"}/>
+            <RickAndMorty name={"Abadango Cluster Princess"} id={6} status={"Alive"} species={"Alien"} gender={"Female"}
+                          image={"https://rickandmortyapi.com/api/character/avatar/6.jpeg"}/>
+        </div>
+    )
 }
 
 export default App;
