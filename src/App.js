d4adda576eebd5fae5dce75763e5b79cee8ee3e7
Dmytrr <dimonpavlo@yahoo.com>
2022-06-21 14:26:17 +0300

    react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import RickAndMorty from "./components/RickAndMorty";
 
 function App() {
     <div className={"wrap"}>
-        <RickAndMorty name={"Rick Sanchez"}  image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
+        <RickAndMorty name={"Rick Sanchez"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
     </div>
 }
 
---
+++ src/components/RickAndMorty.js
@@ -0,0 +1,14 @@
+export default function RickAndMorty(props) {
+    const {name, id, status, species, gender, image} = props;
+    const char = 'character';
+    return (
+        <div className={char}>
+            <h2>{name}</h2>
+            <h3>{id}</h3>
+            <h3>{status}</h3>
+            <h3>{species}</h3>
+            <h3>{gender}</h3>
+            <img src={image}/>
+        </div>
+    );
+}
