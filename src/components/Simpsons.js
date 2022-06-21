5793d97baea992470532d74a9ba08f8fbd9975d2
Dmytrr <dimonpavlo@yahoo.com>
2022-06-21 03:38:24 +0300

    react-hw-1

---
+++ src/components/Simpsons.js
@@ -0,0 +1,11 @@
+export default function Simpsons(props) {
+    const {name, source} = props;
+    const char = 'character';
+    return (
+        <div className={char}>
+            <h2>{name}</h2>
+            <img src={source}/>
+        </div>
+    );
+}
+
