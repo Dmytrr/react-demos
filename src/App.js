d38586ee109edf01b85a6abcb6e333fd2cb885b5
Dmytrr <dimonpavlo@yahoo.com>
2022-06-21 12:17:02 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import './components/Simpsons';
+import Simpsons from "./components/Simpsons";
 
 function App() {
-
+    return (
+        <div className="wrap">
+            <Simpsons name={'Homer'}
+                      source={'https://i.pinimg.com/originals/36/f3/76/36f376dcc85528273912ba4cfb54a1f2.png'}/>
+            <Simpsons name={'Marge'}
+                      source={'https://upload.wikimedia.org/wikipedia/ru/thumb/0/0b/Marge_Simpson.png/217px-Marge_Simpson.png'}/>
+            <Simpsons name={'Bart'} source={'https://slovnet.ru/wp-content/uploads/2019/01/3-2.png'}/>
+            <Simpsons name={'Lisa'}
+                      source={'https://i.pinimg.com/originals/03/05/1e/03051e7071e4d8a8929dcee946244e7d.png'}/>
+            <Simpsons name={'Maggie'} source={'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'}/>
+        </div>
+    );
 }
 
 export default App;
