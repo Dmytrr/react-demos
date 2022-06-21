c06cc1fb1995e3426cb2cb72103d63cb11ef93cd
Dmytrr <dimonpavlo@yahoo.com>
2022-06-21 14:46:02 +0300

    react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import RickAndMorty from "./components/RickAndMorty";
 
 function App() {
     <div className={"wrap"}>
-        <RickAndMorty name={"Rick Sanchez"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
+        <RickAndMorty name={"Rick Sanchez"} id={1} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
     </div>
 }
 
