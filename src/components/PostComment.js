1bb201554f77861c0e1310b38f487fce3ddf2525
Dmytrr <dimonpavlo@yahoo.com>
2022-06-30 14:01:02 +0300

    react-hw-4

--- src/components/PostComment.js
+++ src/components/PostComment.js
@@ -2,9 +2,9 @@ import {useForm} from "react-hook-form";
 
 export default function PostComment() {
 
-    let {register, handleSubmit, formState: {errors}} = useForm({defaultValues: {userId: '1', title: 'title', body: 'hello, world!'}});
+    let {register, handleSubmit, formState: {errors}} = useForm({defaultValues: {postId: '1', name: 'Maria', body: 'hello, world!'}});
     let submit = (obj) => {
-        fetch('https://jsonplaceholder.typicode.com/posts', {
+        fetch('https://jsonplaceholder.typicode.com/comments', {
             method: 'POST',
             body: JSON.stringify(obj),
             headers: {
@@ -18,10 +18,10 @@ export default function PostComment() {
     return (
         <div>
             <form onSubmit={handleSubmit(submit)}>
-                <input type="text"{...register('userId', {required: true})}/>
-                {errors.userId && <span>userId is required</span>}
-                <input type="text"{...register('title', {required: true})}/>
-                {errors.title && <span>title is required</span>}
+                <input type="text"{...register('postId', {required: true})}/>
+                {errors.postId && <span>userId is required</span>}
+                <input type="text"{...register('name', {required: true})}/>
+                {errors.name && <span>name is required</span>}
                 <input type="text"{...register('body', {required: true})}/>
                 {errors.body && <span>post is required</span>}
                 <input type="submit"/>
